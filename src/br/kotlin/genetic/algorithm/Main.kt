5187e692650ae3d67cc8dca4f1c614718d87321f
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-09-24 12:08:27 -0300

    First commit

---
+++ src/br/kotlin/genetic/algorithm/Main.kt
@@ -0,0 +1,11 @@
+package br.kotlin.genetic.algorithm
+
+fun main(args: Array<String>) {
+    val data = Product(
+            name = "Arroz Doce",
+            price = 15.00,
+            size = 3.00
+    )
+
+    System.out.println(data)
+}
---
+++ src/br/kotlin/genetic/algorithm/Product.kt
@@ -0,0 +1,7 @@
+package br.kotlin.genetic.algorithm
+
+data class Product (
+        val name : String,
+        val price : Double,
+        val size : Double
+)
